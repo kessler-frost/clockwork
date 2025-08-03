0ec85117ffafbc9fb43c7f19a5d4548d684502ef
fimbulwinter <sanandsankalp@gmail.com>
2025-08-02 23:19:21 -0400

    updating definition

--- aquila/main.py
+++ aquila/main.py
@@ -5,10 +5,25 @@
 # adjustable. And in order for building things deterministically we need to start small -- with tasks which are not "intelligent"
 # and then increase their complexity by combining them with other "dumb" tasks and slowly inject intelligence into them.
 
-# ------ Conceptually, essentially how functional programming works -------
+# ------ Conceptually, essentially how functional programming works when it comes to building intelligent task framework -------
 
 # Another feature/mechanism I want to have is essentially converting your "prompt" or your "discussion" (including "context")
 # into a declarative and deterministic function that is converted into a non-human-readable binary, that is reusable
 # in a completely different context BUT in a similar manner.
 
-# The objective is to build a large assortment of tasks as functions. For example, a function ``
+# The objective is to build a large assortment of tasks as functions that developers can just use to 
+# simplify building bigger things - reducing friction, if not completely eliminating it, for technical details
+# like a language, framework, etc., but giving them that in a package that they are comfortable with like say a typescript sdk.
+# 
+# I want to build a factory of lego blocks.
+# 
+# For example, (** = <sensible but variable number of inputs everytime>)
+# level 1 intelligent task function is `eat_food(**)`, 
+# level 2 is `eat_food_to_grow(**)`,
+# level 3 is `eat_food_to_grow_and_kill_monster(**)`
+#
+# An adjacent function for that would be
+# `eat_food_to_grow_and_pick_up_a_car(**)`
+#
+#
+# "Agents" are just a medium of task resolution and not always the right answer
