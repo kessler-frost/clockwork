77a10b632a2dc9834c4aae5dfb577ed3fb3fec5b
fimbulwinter <sanandsankalp@gmail.com>
2025-08-02 23:34:05 -0400

    defining what  means

--- aquila/engines/declaration.py
+++ aquila/engines/declaration.py
@@ -1,1 +1,9 @@
-# Declarative code compilation
+# Declaratively defining the intelligent tasks and how exactly will it operate on the state to achieve the desired
+# state (which is also specified here) from the current state.
+#
+# These declarations will be compiled to a binary and then executed as/when neccessary
+#
+# A LOT of inspiration from terraform and it's HCL language
+#
+# Also need to have a way to perform ** evaluation **, to check whether it is able to reliably achieve what was wanted
+# ********* -> this could be a potential source of revenue as well
--- aquila/engines/station.py
+++ aquila/engines/station.py
@@ -1,2 +1,3 @@
 # State Management for provisioning intelligent tasks - also possibly including the
-# state of the environment they will be operating upon
+# state of the environment they will be operating upon.
+# Includes updating the state, reverting the state, backing up the state, etc.
