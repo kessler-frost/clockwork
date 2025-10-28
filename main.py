a3bbb6c773a021a6151922aa4f09be3cb32f9a81
fimbulwinter <sanandsankalp@gmail.com>
2025-10-27 21:02:19 -0400

    cleanup

--- main.py
+++
@@ -1,7 +0,0 @@
-from clockwork.resources import FileResource
-
-article = FileResource(
-    name="article.md",
-    description="Write about the Fibonacci sequence and its mathematical properties",
-    directory="output",
-)
